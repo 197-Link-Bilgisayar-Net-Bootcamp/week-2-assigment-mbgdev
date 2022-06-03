39d7d11f0f0e186b74fe210a6b5f0896e22f9369
mbgdev <mb.gurener@gmail.com>
2022-06-03 19:02:38 +0300

    first

---
+++ Link.Odev/Link.Odev.Core/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Link.Odev.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+        void Commit();
+
+
+    }
+}
